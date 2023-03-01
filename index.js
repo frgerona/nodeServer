756dc853f297d947956f6feb265b86a8c7454ffd
frgerona <frgerona@gmail.com>
2023-03-01 13:06:41 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// import your http
+const http = require('http')
+
+// create server with HTTP
+const server = http.createServer((req, res)=> {
+    console.log('server is created')
+})
+
+// initiate port
+const PORT = 4000;
+
+// listen to server
+server.listen(PORT, ()=> console.log(`server is running on port ${PORT}`))
